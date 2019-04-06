76c5ab804f5fc3e68f85fdef7744569b2d5d1734
chris_gong <chris_gong@yahoo.com>
2019-04-06 11:47:10 -0400

    added physics bodies to ball and floor and a pan gesture method to kick the ball

--- FlickSoccer/GameViewController.swift
+++ FlickSoccer/GameViewController.swift
@@ -18,6 +18,8 @@ class GameViewController: UIViewController {
     var ballNode: SCNNode!
     var cameraNode: SCNNode!
     
+    var fingerStartingPosition: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,8 +29,39 @@ class GameViewController: UIViewController {
         
         ballNode = scene.rootNode.childNode(withName: "ball", recursively: true)
         cameraNode = scene.rootNode.childNode(withName: "camera", recursively: true)
+        
+        fingerStartingPosition = CGPoint(x: 0, y: 0)
+        let panGestureReocgnizer = UIPanGestureRecognizer(target: self, action: #selector(kickBall(_:)))
+        sceneView.addGestureRecognizer(panGestureReocgnizer)
     }
 
+    @objc func kickBall(_ gesture: UIPanGestureRecognizer) {
+        guard gesture.view != nil else {return} // pan gesture needs to occur on an actual view
+        //print("pan gesture recognized")
+        if gesture.state == .began {
+            // save the original position of the user's finger
+            print("pan gesture started")
+            fingerStartingPosition = gesture.location(in: sceneView)
+        }
+        else if gesture.state == .ended {
+            // get the position of the release of the user's finger
+            // decide whether the ball should be kicked by either
+            // checking the amount of time passed between finger on and finger off
+            // checking if the swipe was long enough
+            // check if the swipe made contact with the ball
+            // check if the ball has already been kicked (not touching the ground)
+            // and checking if the swipe goes forwards
+            print("pan gesture ended")
+            let fingerEndingPosition = gesture.location(in: sceneView)
+            if fingerStartingPosition.y > fingerEndingPosition.y {
+                // calculate force
+                let forceVector = SCNVector3(x: Float(fingerEndingPosition.x - fingerStartingPosition.x) * 0.01, y: Float(fingerStartingPosition.y - fingerEndingPosition.y) * 0.01, z: -5)
+                ballNode.physicsBody?.applyForce(forceVector, asImpulse: true)
+                fingerStartingPosition = CGPoint(x: 0, y: 0) // reset starting position (probably not necessary)
+            }
+        }
+        
+    }
     override var shouldAutorotate: Bool {
         return false
     }
